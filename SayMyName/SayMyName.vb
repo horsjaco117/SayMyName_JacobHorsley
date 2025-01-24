03f3d9ba08fcf77bf9c282b5dc731ac421d42c53
horsjaco117 <jacob_horsley@live.com>
2025-01-23 18:42:04 -0700

    Added options and renamed module

---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,28 @@
+﻿'Jacob Horsley
+'Spring 2025
+'RCET 2265
+'SayMyName
+'
+
+Option Explicit On
+Option Strict On
+
+
+Module SayMyName 'Figure out how to rename this
+
+    Sub Main()
+
+
+
+        Console.WriteLine("What is your Name?")
+        Console.WriteLine()
+
+        Console.WriteLine("Jacob")
+        Console.WriteLine()
+
+        Console.WriteLine("Hello there, Jacob")
+
+
+    End Sub
+
+End Module
